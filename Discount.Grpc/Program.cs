a2cefac9dfb7597abd5c63ef535e3e9542c11eac
Thomaz Henrique <treimberg7@gmail.com>
2024-08-13 19:40:39 -0300

    Creating gprc project

---
+++ Discount.Grpc/Program.cs
@@ -0,0 +1,16 @@
+using Discount.Grpc.Repository.Interfaces;
+using Discount.Grpc.Repository;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+builder.Services.AddGrpc();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
