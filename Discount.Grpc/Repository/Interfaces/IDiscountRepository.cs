8809dd63278e4baf0905b3a5a285ab2178b67e2e
Thomaz Henrique <treimberg7@gmail.com>
2024-08-13 19:41:13 -0300

    Adding required classes

---
+++ Discount.Grpc/Repository/Interfaces/IDiscountRepository.cs
@@ -0,0 +1,12 @@
+﻿using Discount.Grpc.Entities;
+
+namespace Discount.Grpc.Repository.Interfaces
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetByProductName(string productName);
+        Task<bool> Create(Coupon coupon);
+        Task<bool> Update(Coupon coupon);
+        Task<bool> Delete(string productName);
+    }
+}
